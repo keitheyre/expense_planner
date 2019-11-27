955c82aacf159aa48837975a3374fbff7c9dc731
keitheyre <keith.eyre91@gmail.com>
2019-11-27 23:10:22 +0000

    Change Column to ListView to use Build-In scrolling Column

--- lib/main.dart
+++ lib/main.dart
@@ -26,21 +26,19 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Expense App"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text("Chart Goes here"),
-                elevation: 5,
-              ),
+      body: ListView(
+        //mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text("Chart Goes here"),
+              elevation: 5,
             ),
-            UserTransactions(),
-          ],
-        ),
+          ),
+          UserTransactions(),
+        ],
       ),
     );
   }
