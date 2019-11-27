4f429463c023532050020a292462fd9839b0471f
keitheyre <keith.eyre91@gmail.com>
2019-11-27 21:11:28 +0000

    Adding 2 items to a Column to simulate a list. Style each part of the widgets. Change to look different than instructors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Expense App',
       home: MyHomePage(),
     );
   }
@@ -16,22 +18,79 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<Transaction> transactions = [
+      Transaction(
+          id: "001", title: "New Shoes", amount: 25.99, date: DateTime.now()),
+      Transaction(
+          id: "002", title: "Groceries", amount: 63.99, date: DateTime.now()),
+    ];
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Expense App"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Card(
-              child: Text("Chart"),
+      appBar: AppBar(
+        title: Text("Expense App"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text("Chart Goes here"),
               elevation: 5,
             ),
-            Card(
-              child: Text("List of TX"),
-            )
-          ],
-
-
-        ));
+          ),
+          Column(
+              children: transactions.map((transaction) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  //! Container for the amount on the left
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      transaction.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(20),
+                  ),
+                  //! Column for the title and the date ontop of one another
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transaction.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 30
+                        ),
+                      ),
+                      Text(
+                        transaction.date.toString(),
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }).toList()),
+        ],
+      ),
+    );
   }
 }
