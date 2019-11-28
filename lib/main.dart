fa45965390ee9a32be60e6190dc47d5d50b9d6d5
keitheyre <keith.eyre91@gmail.com>
2019-11-28 20:41:26 +0000

    Add a plus icon to the AppBar. Add a FloatingActionButton and center it horizontally

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expense App"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: ListView(
         children: <Widget>[
@@ -33,6 +39,11 @@ class MyHomePage extends StatelessWidget {
           ),
           UserTransactions(),
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
